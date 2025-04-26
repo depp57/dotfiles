38c5291de6dafae541c2104d9e1f4517553f274c
Sacha Thommet <deppdu5758@gmail.com>
2025-04-26 17:21:56 +0200

    automatically ensure packages are installed using ansible

---
+++ run_once_install_ansible.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# https://docs.ansible.com/ansible/latest/installation_guide/installation_distros.html#installing-ansible-on-ubuntu
+install_on_ubuntu() {
+  sudo apt update
+  sudo apt install software-properties-common
+  sudo add-apt-repository --yes --update ppa:ansible/ansible
+  sudo apt install ansible
+}
+
+OS="$(uname -s)"
+case "${OS}" in
+    Linux*)
+        if [ -f /etc/lsb-release ]; then
+            install_on_ubuntu
+        else
+            echo "Unsupported Linux distribution"
+            exit 1
+        fi
+        ;;
+esac
+
+ansible-playbook ~/.bootstrap-ansible/setup.yml --ask-become-pass
+
+echo "Ansible installation complete."
